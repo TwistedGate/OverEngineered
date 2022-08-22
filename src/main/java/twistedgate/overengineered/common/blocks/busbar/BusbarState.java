8848ff67965ab0c25c460f60ef7c93b61414df32
TwistedGate <31029668+TwistedGate@users.noreply.github.com>
2022-08-22 17:14:03 +0200

    Mild Cleanup
    Recursion problem still exists, and i don't know why and how.
    And it's realy frustrating and demotivating trying to find *something*
    that maaay be the cause but always ending up not finding it.

--- src/main/java/twistedgate/overengineered/common/blocks/busbar/BusbarState.java
+++ src/main/java/twistedgate/overengineered/common/blocks/busbar/BusbarState.java
@@ -7,11 +7,7 @@ import javax.annotation.Nullable;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.BaseRailBlock;
-import net.minecraft.world.level.block.RailBlock;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.properties.RailShape;
-import twistedgate.overengineered.OverEngineered;
 import twistedgate.overengineered.utils.enums.EnumBusbarShape;
 
 public class BusbarState{
@@ -32,6 +28,17 @@ public class BusbarState{
 		updateConnections(shape);
 	}
 	
+	@Nullable
+	private BusbarState getBus(BlockPos pos){
+		BlockState blockState = this.level.getBlockState(pos);
+		// TODO May need special cases for edges
+		return BusbarBlock.isBusbar(blockState) ? new BusbarState(this.level, pos, blockState) : null;
+	}
+	
+	public BlockState getState(){
+		return this.state;
+	}
+	
 	private boolean canConnectTo(BusbarState busState){
 		return connectsTo(busState) || this.connections.size() != 2;
 	}
@@ -40,37 +47,10 @@ public class BusbarState{
 		return hasConnection(busState.pos);
 	}
 	
-	@Nullable
-	private BusbarState getBus(BlockPos pos){
-		BlockState blockState = this.level.getBlockState(pos);
-		// TODO May need special cases for edges. (Left "original" below)
-		return BusbarBlock.isBusbar(blockState) ? new BusbarState(this.level, pos, blockState) : null;
-		
-		/*
-		if(BusbarBlock.isBusbar(blockState)){
-			return new BusbarState(this.level, pos, blockState);
-		}else{
-			BlockPos other = pos.above();
-			blockState = this.level.getBlockState(other);
-			if(BusbarBlock.isBusbar(blockState)){
-				return new BusbarState(this.level, other, blockState);
-			}else{
-				other = pos.below();
-				blockState = this.level.getBlockState(other);
-				return BusbarBlock.isBusbar(blockState) ? new BusbarState(this.level, other, blockState) : null;
-			}
-		}
-		*/
-	}
-	
-	public BlockState getState(){
-		return this.state;
-	}
-	
 	private boolean hasConnection(BlockPos pos){
 		for(int i = 0;i < this.connections.size();++i){
 			BlockPos blockpos = this.connections.get(i);
-			if(blockpos.getX() == pos.getX() && blockpos.getZ() == pos.getZ()){
+			if(blockpos.equals(pos)){
 				return true;
 			}
 		}
@@ -87,20 +67,21 @@ public class BusbarState{
 		return busState.canConnectTo(this);
 	}
 	
+	@SuppressWarnings("unused")
 	public BusbarState place(boolean placing, EnumBusbarShape shapeIn){
-		BlockPos posDown = this.pos.below();
-		BlockPos posUp = this.pos.above();
-		BlockPos posNorth = this.pos.north();
-		BlockPos posEast = this.pos.east();
-		BlockPos posSouth = this.pos.south();
-		BlockPos posWest = this.pos.west();
+		final BlockPos posDown = this.pos.below();
+		final BlockPos posUp = this.pos.above();
+		final BlockPos posNorth = this.pos.north();
+		final BlockPos posEast = this.pos.east();
+		final BlockPos posSouth = this.pos.south();
+		final BlockPos posWest = this.pos.west();
 		
-		boolean hasDown = hasNeighborBus(posDown);
-		boolean hasUp = hasNeighborBus(posUp);
-		boolean hasNorth = hasNeighborBus(posNorth);
-		boolean hasEast = hasNeighborBus(posEast);
-		boolean hasSouth = hasNeighborBus(posSouth);
-		boolean hasWest = hasNeighborBus(posWest);
+		final boolean hasDown = hasNeighborBus(posDown);
+		final boolean hasUp = hasNeighborBus(posUp);
+		final boolean hasNorth = hasNeighborBus(posNorth);
+		final boolean hasEast = hasNeighborBus(posEast);
+		final boolean hasSouth = hasNeighborBus(posSouth);
+		final boolean hasWest = hasNeighborBus(posWest);
 		
 		EnumBusbarShape shape = null;
 		
@@ -188,162 +169,23 @@ public class BusbarState{
 		return this;
 	}
 	
-	/** Temporary, just for peaking */
-	@Deprecated(forRemoval = true)
-	public void /*RailState*/ place(){
-		boolean pPowered = false, pPlaceBlock = false, canMakeSlopes = false;
-		RailShape pShape = null;
-		// Above would be the method args
-		//-------------------------------------------------------------------------
-		
-		BlockPos posNorth = this.pos.north();
-		BlockPos posSouth = this.pos.south();
-		BlockPos posWest = this.pos.west();
-		BlockPos posEast = this.pos.east();
-		boolean hasNorth = this.hasNeighborBus(posNorth);
-		boolean hasSouth = this.hasNeighborBus(posSouth);
-		boolean hasWest = this.hasNeighborBus(posWest);
-		boolean hasEast = this.hasNeighborBus(posEast);
-		boolean ns = hasNorth || hasSouth;
-		boolean ew = hasWest || hasEast;
-		
-		RailShape railshape = null;
-		
-		if(ns && !ew){
-			railshape = RailShape.NORTH_SOUTH;
-		}
-		
-		if(ew && !ns){
-			railshape = RailShape.EAST_WEST;
-		}
-		
-		boolean se = hasSouth && hasEast;
-		boolean sw = hasSouth && hasWest;
-		boolean ne = hasNorth && hasEast;
-		boolean nw = hasNorth && hasWest;
-		if(!this.isStraight){
-			if(se && !hasNorth && !hasWest){
-				railshape = RailShape.SOUTH_EAST;
-			}
-			
-			if(sw && !hasNorth && !hasEast){
-				railshape = RailShape.SOUTH_WEST;
-			}
-			
-			if(nw && !hasSouth && !hasEast){
-				railshape = RailShape.NORTH_WEST;
-			}
-			
-			if(ne && !hasSouth && !hasWest){
-				railshape = RailShape.NORTH_EAST;
-			}
-		}
-		
-		if(railshape == null){
-			if(ns && ew){
-				railshape = pShape;
-			}else if(ns){
-				railshape = RailShape.NORTH_SOUTH;
-			}else if(ew){
-				railshape = RailShape.EAST_WEST;
-			}
-			
-			if(!this.isStraight){
-				if(pPowered){
-					if(se){
-						railshape = RailShape.SOUTH_EAST;
-					}
-					
-					if(sw){
-						railshape = RailShape.SOUTH_WEST;
-					}
-					
-					if(ne){
-						railshape = RailShape.NORTH_EAST;
-					}
-					
-					if(nw){
-						railshape = RailShape.NORTH_WEST;
-					}
-				}else{
-					if(nw){
-						railshape = RailShape.NORTH_WEST;
-					}
-					
-					if(ne){
-						railshape = RailShape.NORTH_EAST;
-					}
-					
-					if(sw){
-						railshape = RailShape.SOUTH_WEST;
-					}
-					
-					if(se){
-						railshape = RailShape.SOUTH_EAST;
-					}
-				}
-			}
-		}
-		
-		if(railshape == RailShape.NORTH_SOUTH && canMakeSlopes){
-			if(BaseRailBlock.isRail(this.level, posNorth.above())){
-				railshape = RailShape.ASCENDING_NORTH;
-			}
-			
-			if(BaseRailBlock.isRail(this.level, posSouth.above())){
-				railshape = RailShape.ASCENDING_SOUTH;
-			}
-		}
-		
-		if(railshape == RailShape.EAST_WEST && canMakeSlopes){
-			if(BaseRailBlock.isRail(this.level, posEast.above())){
-				railshape = RailShape.ASCENDING_EAST;
-			}
-			
-			if(BaseRailBlock.isRail(this.level, posWest.above())){
-				railshape = RailShape.ASCENDING_WEST;
-			}
-		}
-		
-		if(railshape == null){
-			railshape = pShape;
-		}
-		
-		//updateConnections(railshape);
-		this.state = this.state.setValue(RailBlock.SHAPE, railshape);
-		if(pPlaceBlock || this.level.getBlockState(this.pos) != this.state){
-			this.level.setBlock(this.pos, this.state, 3);
-			
-			for(int i = 0;i < this.connections.size();++i){
-				BusbarState busState = getBus(this.connections.get(i));
-				if(busState != null){
-					busState.removeSoftConnections();
-					if(busState.canConnectTo(this)){
-						busState.connectTo(this);
-					}
-				}
-			}
-		}
-		
-		//return this;
-	}
-	
+	@SuppressWarnings("unused")
 	private void connectTo(BusbarState state){
 		this.connections.add(state.pos);
 		
-		BlockPos posDown = this.pos.below();
-		BlockPos posUp = this.pos.above();
-		BlockPos posNorth = this.pos.north();
-		BlockPos posEast = this.pos.east();
-		BlockPos posSouth = this.pos.south();
-		BlockPos posWest = this.pos.west();
+		final BlockPos posDown = this.pos.below();
+		final BlockPos posUp = this.pos.above();
+		final BlockPos posNorth = this.pos.north();
+		final BlockPos posEast = this.pos.east();
+		final BlockPos posSouth = this.pos.south();
+		final BlockPos posWest = this.pos.west();
 		
-		boolean conDown = hasConnection(posDown);
-		boolean conUp = hasConnection(posUp);
-		boolean conNorth = hasConnection(posNorth);
-		boolean conSouth = hasConnection(posSouth);
-		boolean conWest = hasConnection(posWest);
-		boolean conEast = hasConnection(posEast);
+		final boolean conDown = hasConnection(posDown);
+		final boolean conUp = hasConnection(posUp);
+		final boolean conNorth = hasConnection(posNorth);
+		final boolean conSouth = hasConnection(posSouth);
+		final boolean conWest = hasConnection(posWest);
+		final boolean conEast = hasConnection(posEast);
 		
 		EnumBusbarShape shape = null;
 		
@@ -376,107 +218,8 @@ public class BusbarState{
 			shape = EnumBusbarShape.INSULATORS_DOWN_NORTH_SOUTH;
 		}
 		
-		OverEngineered.log.debug(
-			"DEBUG\r\nconnectTo(\r\n\t" +
-			"Level: {}\r\n\t" +
-			"BlockState: {}\r\n\t" +
-			"Pos: {}\r\n\t" +
-			"Connections: {}\r\n)"+
-			"{\r\n\t" +
-			"Level: {}\r\n\t" +
-			"State: {}\r\n\t" +
-			"Pos: {}\r\n\t" +
-			"Connections: {}\r\n\t" +
-			"Shape: {}\r\n" +
-			"}",
-				state.level,
-				state.state,
-				state.pos,
-				state.connections,
-				
-				this.level,
-				this.state,
-				this.pos,
-				this.connections,
-				shape
-		);
-		
 		this.state = this.state.setValue(BusbarBlock.SHAPE, shape);
 		this.level.setBlock(this.pos, this.state, 3);
-	}
-
-	/** Temporary, just for peaking */
-	@SuppressWarnings({"null", "unused"})
-	@Deprecated(forRemoval = true)
-	private void connectTo(){
-		BusbarState pState = null;
-		// Above would be the method args
-		//-------------------------------------------------------------------------
-		
-		this.connections.add(pState.pos);
-		BlockPos posNorth = this.pos.north();
-		BlockPos posSouth = this.pos.south();
-		BlockPos posWest = this.pos.west();
-		BlockPos posEast = this.pos.east();
-		boolean conNorth = hasConnection(posNorth);
-		boolean conSouth = hasConnection(posSouth);
-		boolean conWest = hasConnection(posWest);
-		boolean conEast = hasConnection(posEast);
-		RailShape shape = null;
-		if(conNorth || conSouth){
-			shape = RailShape.NORTH_SOUTH;
-		}
-		
-		if(conWest || conEast){
-			shape = RailShape.EAST_WEST;
-		}
-		
-		if(!this.isStraight){
-			if(conSouth && conEast && !conNorth && !conWest){
-				shape = RailShape.SOUTH_EAST;
-			}
-			
-			if(conSouth && conWest && !conNorth && !conEast){
-				shape = RailShape.SOUTH_WEST;
-			}
-			
-			if(conNorth && conWest && !conSouth && !conEast){
-				shape = RailShape.NORTH_WEST;
-			}
-			
-			if(conNorth && conEast && !conSouth && !conWest){
-				shape = RailShape.NORTH_EAST;
-			}
-		}
-		
-		/*
-		if(railshape == RailShape.NORTH_SOUTH && canMakeSlopes){
-			if(BaseRailBlock.isRail(this.level, blockpos.above())){
-				railshape = RailShape.ASCENDING_NORTH;
-			}
-			
-			if(BaseRailBlock.isRail(this.level, blockpos1.above())){
-				railshape = RailShape.ASCENDING_SOUTH;
-			}
-		}
-		
-		if(railshape == RailShape.EAST_WEST && canMakeSlopes){
-			if(BaseRailBlock.isRail(this.level, blockpos3.above())){
-				railshape = RailShape.ASCENDING_EAST;
-			}
-			
-			if(BaseRailBlock.isRail(this.level, blockpos2.above())){
-				railshape = RailShape.ASCENDING_WEST;
-			}
-		}
-		*/
-		
-		if(shape == null){
-			shape = RailShape.NORTH_SOUTH;
-		}
-		
-//		this.state = this.state.setValue(BusbarBlock.SHAPE, railshape);
-//		this.level.setBlock(this.pos, this.state, 3);
 	}
 	
 	private void removeSoftConnections(){
