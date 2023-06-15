e1407c911cb0387bd73119a766f16c013f52786a
TwistedGate <31029668+TwistedGate@users.noreply.github.com>
2023-06-15 13:21:22 +0200

    Completely forgot `OERegisters` is a thing.
    Im very dumb sometimes..

--- src/main/java/twistedgate/overengineered/common/OEContent.java
+++ src/main/java/twistedgate/overengineered/common/OEContent.java
@@ -1,35 +1,8 @@
 package twistedgate.overengineered.common;
 
-import java.util.function.Supplier;
-
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.event.lifecycle.ParallelDispatchEvent;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
-import twistedgate.overengineered.OverEngineered;
-import twistedgate.overengineered.common.blocks.tileentity.TileTypes;
 
 public class OEContent{
-	private static final DeferredRegister<Block> BLOCK_REGISTER = DeferredRegister.create(ForgeRegistries.BLOCKS, OverEngineered.MODID);
-	private static final DeferredRegister<Item> ITEM_REGISTER = DeferredRegister.create(ForgeRegistries.ITEMS, OverEngineered.MODID);
-	
-	public static final void addRegistersToEventBus(IEventBus eventBus){
-		BLOCK_REGISTER.register(eventBus);
-		ITEM_REGISTER.register(eventBus);
-		TileTypes.REGISTER.register(eventBus);
-	}
-	
-	protected static final <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> constructor){
-		return BLOCK_REGISTER.register(name, constructor);
-	}
-	
-	protected static final <T extends Item> RegistryObject<T> registerItem(String name, Supplier<T> constructor){
-		return ITEM_REGISTER.register(name, constructor);
-	}
-	
 	public static class Blocks{
 		
 		private static void forceClassLoad(){
---
+++ src/main/java/twistedgate/overengineered/common/OETileTypes.java
@@ -0,0 +1,7 @@
+package twistedgate.overengineered.common;
+
+public class OETileTypes{
+	
+	public static void forceClassLoad(){
+	}
+}
